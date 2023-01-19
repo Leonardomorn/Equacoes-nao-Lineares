3e41f6b0e0a0e727f547016b891d528ef1fab479
Leonardomorn <lsc@localhost.localdomain>
2023-01-19 16:00:56 -0300

    modelo moodle

---
+++ ZeroFuncao.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+#include "utils.h"
+#include "ZeroFuncao.h"
+
+double newtonRaphson (Polinomio p, double x0, double eps,
+		   int *it, double *raiz)
+{
+
+}
+
+
+double secante (Polinomio p, double x0, double x1, double eps,
+	     int *it, double *raiz)
+{
+
+}
+
+
+void calcPolinomio_rapido(Polinomio p, double x, double *px, double *dpx)
+{
+
+}
+
+
+void calcPolinomio_lento(Polinomio p, double x, double *px, double *dpx)
+{
+
+}
