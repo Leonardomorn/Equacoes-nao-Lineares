f2a52dd1635964fb05d1623d8f4b00ee6c6c7502
camaleoao <camargo.leonardo1@gmail.com>
2023-01-22 10:23:35 -0300

    implementados os calculos de polinomios e derivadas, adicionado newtonraphson nao testado

--- ZeroFuncao.c
+++ ZeroFuncao.c
@@ -4,10 +4,42 @@
 #include "utils.h"
 #include "ZeroFuncao.h"
 
+/*!
+	\brief funcao que retorna o erro relativo aproximado entre dois numeros
+	\param xProx é o próximo x da iteração
+	\param xi é o x atual da iteração
+*/
+double erro_relativo_aproximado(double xProx, double xi)
+{
+	double erro = ((xProx - xi) / xProx) * 100;
+	return erro;
+}
+
+/*!
+	\brief funcao que aplica o método newtonRaphson para descobrir uma raiz de um 
+	polinômio de grau n e retorna o erro relativo aproximado
+	\param p estrutura que possui os coeficientes do polinômio e o grau
+	\param x0 chute inicial
+	\param eps tolerância máxima
+	\param *it número de iterações
+	\param *raiz raiz do polinomio 
+	\return erro relativo aproximado
+*/
 double newtonRaphson (Polinomio p, double x0, double eps,
 		   int *it, double *raiz)
 {
-
+	double erro = 1.0 + eps;
+	double xi = x0; double xProx; double px, dpx;
+	*it = 0;
+	while (it < MAXIT && erro > eps) 
+	{
+		calcPolinomio_rapido(p,xi, &px, &dpx);
+		xProx = xi - (px / dpx);
+		erro = erro_relativo_aproximado(xProx, xi);
+	}
+	
+	*raiz = xProx;
+	return erro;
 }
 
 
@@ -20,11 +52,28 @@ double secante (Polinomio p, double x0, double x1, double eps,
 
 void calcPolinomio_rapido(Polinomio p, double x, double *px, double *dpx)
 {
-
+	double pxAux = 0.0, dpxAux = 0.0;
+	for (int i = p.grau; i > 0; --i)
+	{
+		pxAux = pxAux * x  + p.p[i];
+		dpxAux = dpxAux * x + pxAux;
+	}
+	pxAux = pxAux * x + p.p[0];
+	*px = pxAux;
+	*dpx = dpxAux;
 }
 
 
 void calcPolinomio_lento(Polinomio p, double x, double *px, double *dpx)
 {
+	double pxAux = 0.0, dpxAux = 0.0;
 
+	for (int i = p.grau; i > 0; --i)
+	{
+		pxAux = pxAux + p.p[i] * pow(x,i);
+		dpxAux = dpxAux + p.p[i] * i * pow(x, (i-1) );
+	}
+	pxAux = pxAux + p.p[0];
+	*px = pxAux;
+	*dpx = dpxAux;
 }
--- ZeroFuncao.h
+++ ZeroFuncao.h
@@ -10,16 +10,20 @@
 #define MAXIT 500
 #define EPS 1.0e-8
 
+/*!
+  \brief Estrutura de um polinomio 
+  \param grau é o grau do polinomio
+  \param p é o vetor dos coeficientes do polinômio
+*/
 typedef struct {
   double *p;
   int grau;
 } Polinomio;
 
-// Métodos
+
 double newtonRaphson (Polinomio p, double x0, double eps, int *it, double *raiz);
 double secante (Polinomio p, double x0, double x1, double eps, int *it, double *raiz);
 
-// Cálculo de Polinômios
 void calcPolinomio_rapido(Polinomio p, double x, double *px, double *dpx );
 void calcPolinomio_lento(Polinomio p, double x, double *px, double *dpx );
 
